ac9380be5b405ad86bdebcc8050a657b8ad5b8be
maurya20 <mukesh.ice17@gmail.com>
2020-11-11 18:41:34 +0530

    Login page initial rendering

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { View, Text,StyleSheet } from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
 const Login = () => {
     return (
-        <View>
-            <Text>Loginnn</Text>
+        <View style={styles.backgraoundStyle}>
+            <TextInput style={styles.inputStyle} placeholder="Enter Email"/>
+            <TextInput style={styles.inputStyle} placeholder="Enter Password"/>
+            <TouchableOpacity>
+                <Text style={styles.submitStyle}>Submit</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    backgraoundStyle:{
+            marginTop:1,
+            flex:1,
+            backgroundColor:"gray"
+  },
+        inputStyle: {
+          height: 50,
+          borderWidth: 2,
+          paddingLeft: 10,
+          margin: 20,
+          borderColor: '#009688',
+          backgroundColor: 'white',
+          borderRadius:10,
+          fontSize:24  
+        },
+        submitStyle:{
+            borderRadius:10,
+            fontSize:30,
+            borderWidth: 2,
+            borderColor: 'cyan',
+            marginHorizontal:123,
+            paddingLeft:10,
+            backgroundColor:"blue",
+            color:"white"
+        }
+  })
+
 
 export default Login
